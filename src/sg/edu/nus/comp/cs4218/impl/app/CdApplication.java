1376255d8c52239d592d4ca496e9308aac57fcf2
Ng Zhi Hua <ngzhihua_@hotmail.com>
2017-02-10 21:31:11 +0800

    added cd tests

--- src/sg/edu/nus/comp/cs4218/impl/app/CdApplication.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/CdApplication.java
@@ -1,8 +1,10 @@
 package sg.edu.nus.comp.cs4218.impl.app;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.FileSystems;
 import java.nio.file.Path;
 
 import sg.edu.nus.comp.cs4218.Application;
@@ -27,6 +29,8 @@ public class CdApplication implements Application {
 	private static final String ERROR_EXP_DIRECTORY_UNSPECIFIED = "No relative directory specified";
 	private static final String ERROR_EXP_ONE_ARG = "Only 1 argument is expected";
 	private static final String ERROR_EXP_DIRECTORY_DOES_NOT_EXIST = "Directory does not exist";
+	private static final String ONE_BACK_DIR = "..";
+	private static final String ONE_MORE_BACK_DIR = ".." + System.getProperty("path.separator");
 	
 	/**
 	 * Runs the cd application with the specified arguments.
@@ -58,22 +62,60 @@ public class CdApplication implements Application {
 			throw new CdException(ERROR_EXP_ONE_ARG);
 		}
 		
-		String changedDirectory = Environment.currentDirectory + System.getProperty("path.separator") + args[0];
+		changeDirectory(args[0]);
 		
-		File f = new File(changedDirectory);
-		if (f.exists() && f.isDirectory()){
-			Environment.currentDirectory = Environment.currentDirectory + System.getProperty("path.separator") + args[0];
+//		String changedDirectory = Environment.currentDirectory + System.getProperty("path.separator") + args[0];
+//		
+//		File f = new File(changedDirectory);
+//		if (f.exists() && f.isDirectory()){
+//			Environment.currentDirectory = Environment.currentDirectory + System.getProperty("path.separator") + args[0];
+//		}
+//		else{
+//			throw new CdException(ERROR_EXP_DIRECTORY_DOES_NOT_EXIST);
+//		}
+	}
+
+	
+	/**
+	 * @param arg specifies the directory to change to
+	 */
+	private void changeDirectory(String arg) throws CdException{
+		boolean isDir = false;
+		File changeDirectory = new File(Environment.currentDirectory + File.separator +  arg);
+		System.out.println(Environment.currentDirectory + File.separator +  arg);
+		try {
+			changeDirectory = changeDirectory.getCanonicalFile();
+			System.out.println(changeDirectory);
+		} catch (IOException e) {
+			isDir = false;
+		}
+		
+		if (changeDirectory.isDirectory()){
+			Environment.currentDirectory = changeDirectory.getAbsolutePath();
+			isDir = true;
+			return;
 		}
 		else{
+			changeDirectory = new File(arg);
+			try {
+				changeDirectory = changeDirectory.getCanonicalFile();
+			} catch (IOException e) {
+				isDir = false;
+			}
+			if (changeDirectory.isDirectory()){
+				Environment.currentDirectory = changeDirectory.getAbsolutePath();
+				isDir = true;
+				return;
+			}
+		}
+		
+		if (!isDir){
 			throw new CdException(ERROR_EXP_DIRECTORY_DOES_NOT_EXIST);
 		}
 	}
-
+	
 	/**
-	 * Checks if the current directory referenced by this application is valid or not.
-	 * 
-	 * @return
-	 * 		A boolean value indicating if the current directory referenced by this application is valid or not.
+	 * Returns true if Environment.currentDirectory is valid, false, otherwise 
 	 */
 	private boolean isCurrentDirectoryValid() {
 		File dir = new File(Environment.currentDirectory);
--- src/test/CdApplicationTest.java
+++ src/test/CdApplicationTest.java
@@ -2,58 +2,103 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintStream;
 
+import sg.edu.nus.comp.cs4218.Environment;
 import sg.edu.nus.comp.cs4218.exception.CdException;
 import sg.edu.nus.comp.cs4218.impl.app.CdApplication;
+import sg.edu.nus.comp.cs4218.impl.app.EchoApplication;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CdApplicationTest {
-	
+
 	private InputStream input;
 	private OutputStream output;
-	
-	@Test
-	public void testCdNullArgumentsException() {
-		CdApplication cdApp = new CdApplication();
-		
-		try {
-			cdApp.run(null, input, output);
-		} catch (CdException e) {
-			assertEquals(e.getMessage(), "cd: Null argument detected");
-		}
+	private static CdApplication cdApp;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		cdApp = new CdApplication();
 	}
-	
-	@Test
-	public void testCdDirectoryUnspecifiedException(){
-		CdApplication cdApp = new CdApplication();
+
+	@Test(expected = CdException.class)
+	public void testCdNullArgumentsException() throws CdException {
+		// CdApplication cdApp = new CdApplication();
+
+		cdApp.run(null, input, output);
+	}
+
+	@Test(expected = CdException.class)
+	public void testCdDirectoryUnspecifiedException() throws CdException {
 		String[] args = {};
-		
-		try {
-			cdApp.run(args, input, output);
-		} catch (CdException e){
-			assertEquals(e.getMessage(), "cd: No relative directory specified");
-		}
+
+		cdApp.run(args, input, output);
+	}
+
+	@Test(expected = CdException.class)
+	public void testCdMoreThanOneArgException() throws CdException {
+		String[] args = { "HelloWorldDirectory", "HelloWorld2Directory" };
+
+		cdApp.run(args, input, output);
+	}
+
+	@Test(expected = CdException.class)
+	public void testDirectoryDoesNotExistException() throws CdException {
+		cdApp = new CdApplication();
+		String[] args = { "./directoryDoesNotExist" };
+
+		cdApp.run(args, input, output);
+	}
+
+	@Test
+	public void testCdDirectoryExist() throws CdException {
+		cdApp = new CdApplication();
+		String previousPath = Environment.currentDirectory + File.separator + "randomDirectory";
+		File testFile = new File(previousPath);
+		testFile.mkdir();
+		String[] args = { "randomDirectory" };
+
+		cdApp.run(args, input, output);
+
+		assertEquals(previousPath, Environment.currentDirectory);
+
+		testFile.delete();
 	}
 	
 	@Test
-	public void testCdMoreThanOneArgException(){
-		CdApplication cdApp = new CdApplication();
-		String[] args = {"HelloWorldDirectory", "HelloWorld2Directory"};
-		
-		try{
-			cdApp.run(args, input, output);
-		}catch (CdException e){
-			assertEquals(e.getMessage(), "cd: Only 1 argument is expected");
-		}
+	public void testCdDirectoryRelLocal() throws CdException {
+		cdApp = new CdApplication();
+		String previousPath = Environment.currentDirectory + File.separator + "randomDirectory";
+		File testFile = new File(previousPath);
+		testFile.mkdir();
+		String[] args = { "." + File.separator + "randomDirectory" };
+
+		cdApp.run(args, input, output);
+
+		assertEquals(previousPath, Environment.currentDirectory);
+
+		testFile.delete();
 	}
 	
 	@Test
-	public void testDirectoryDoesNotExistException(){
-		CdApplication cdApp = new CdApplication();
-		String[] args = {};
-		
+	public void testCdDirectoryManyRelative() throws CdException {
+		cdApp = new CdApplication();
+		String previousPath = Environment.currentDirectory + File.separator + "randomDirectory";
+		File testFile = new File(previousPath);
+		testFile.mkdir();
+		String[] args = { "randomDirectory" };
+
+		cdApp.run(args, input, output);
+
+		assertEquals(previousPath, Environment.currentDirectory);
+
+		testFile.delete();
 	}
+
 }
