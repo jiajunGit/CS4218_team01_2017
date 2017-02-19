e2b8d38e0ccf146cd4b8cf08c7bd9a66087b16e7
amirrsyd <md.amir.rasyid@gmail.com>
2017-02-20 06:35:22 +0800

    EF2 tests for wc

---
+++ src/sg/edu/nus/comp/cs4218/impl/app/WcApplication.java
@@ -0,0 +1,65 @@
+package sg.edu.nus.comp.cs4218.impl.app;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import sg.edu.nus.comp.cs4218.app.Wc;
+import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
+
+public class WcApplication implements Wc {
+
+	@Override
+	public void run(String[] args, InputStream stdin, OutputStream stdout) throws AbstractApplicationException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String printCharacterCountInFile(String args) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String printWordCountInFile(String args) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String printNewlineCountInFile(String args) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String printAllCountsInFile(String args) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String printCharacterCountInStdin(String args, InputStream stdin) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String printWordCountInStdin(String args, InputStream stdin) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String printNewlineCountInStdin(String args, InputStream stdin) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String printAllCountsInStdin(String args, InputStream stdin) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
