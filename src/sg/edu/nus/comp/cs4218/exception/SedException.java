069b892681a14dce5ffef4f03caaab77dfbd03d0
jiajunGit <JJGitHub@gmail.com>
2017-02-19 15:55:42 +0800

    Added tests for sed

---
+++ src/sg/edu/nus/comp/cs4218/exception/SedException.java
@@ -0,0 +1,10 @@
+package sg.edu.nus.comp.cs4218.exception;
+
+public class SedException extends AbstractApplicationException {
+
+    private static final long serialVersionUID = -441204558704575L;
+    
+    public SedException(String message) {
+        super("sed: " + message);
+    }
+}
