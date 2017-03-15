0f966f106a93c440bc6ec73e93019d6ff39c79b6
amirrsyd <md.amir.rasyid@gmail.com>
2017-03-16 05:49:14 +0800

    fixed echo cal commandsub test

--- src/test/integration/echo/EchoIntegrationTest.java
+++ src/test/integration/echo/EchoIntegrationTest.java
@@ -122,7 +122,7 @@ public class EchoIntegrationTest {
 	
 	@Test
 	public void testIntegrateCal() throws AbstractApplicationException, ShellException, IOException{
-		String command = "echo `cal`";
+		String command = "echo \"`cal`\"";
 		String expected = "     March 2017      Su Mo Tu We Th Fr Sa "
 				+ "         1  2  3  4  5  6  7  8  9  10 11 "
 				+ "12 13 14 15 16 17 18 19 20 21 22 23 24 25 "
